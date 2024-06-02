b65e19917e93371a9d2210dbf09477116adf31c0
bekturinho <ismanalievbektur4@gmail.com>
2024-06-01 23:28:16 -0500

    Initial Commit

---
+++ CDLAappSwiftUi/CDLAappSwiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  CDLAappSwiftUiApp.swift
+//  CDLAappSwiftUi
+//
+//  Created by fortune cookie on 6/1/24.
+//
+
+import SwiftUI
+
+@main
+struct CDLAappSwiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
